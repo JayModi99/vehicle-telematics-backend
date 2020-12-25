9a0d4813bc6baf91a319a2e8c67ef506fccef9e9
Jay Modi <jayjmodi1999@gmail.com>
2020-12-26 01:20:48 +0530

    First Commit

---
+++ vehicle-telematics-backend/src/main/java/com/vehicletelematics/VehicleTelematicsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vehicletelematics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTelematicsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTelematicsBackendApplication.class, args);
+	}
+
+}
